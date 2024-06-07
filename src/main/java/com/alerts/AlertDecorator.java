96440be7a7b39b621f6f9c9ba449e03d58d58831
Jakub Bujak <jakubb@Jakubs-MacBook-Air.local>
2024-06-07 18:11:00 +0200

    tests week 6

--- src/main/java/com/alerts/AlertDecorator.java
+++ src/main/java/com/alerts/AlertDecorator.java
@@ -1,30 +1,30 @@
-package com.alerts;
+// package com.alerts;
 
-// Base decorator class implementing the Alert interface
-public abstract class AlertDecorator implements Alert {
-    protected Alert alert;
+// // Base decorator class implementing the Alert interface
+// public abstract class AlertDecorator implements Alert {
+//     protected Alert alert;
 
-    public AlertDecorator(Alert alert) {
-        this.alert = alert;
-    }
+//     public AlertDecorator(Alert alert) {
+//         this.alert = alert;
+//     }
 
-    @Override
-    public String getPatientId() {
-        return alert.getPatientId();
-    }
+//     @Override
+//     public String getPatientId() {
+//         return alert.getPatientId();
+//     }
 
-    @Override
-    public String getCondition() {
-        return alert.getCondition();
-    }
+//     @Override
+//     public String getCondition() {
+//         return alert.getCondition();
+//     }
 
-    @Override
-    public long getTimestamp() {
-        return alert.getTimestamp();
-    }
+//     @Override
+//     public long getTimestamp() {
+//         return alert.getTimestamp();
+//     }
 
-    @Override
-    public String getMessage() {
-        return alert.getMessage();
-    }
-}
+//     @Override
+//     public String getMessage() {
+//         return alert.getMessage();
+//     }
+// }
